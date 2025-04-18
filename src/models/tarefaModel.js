e8852bd1e7015668a89b16bb6e72b09073fe12fb
pablodelgado26 <158165561+pablodelgado26@users.noreply.github.com>
2025-04-18 14:25:36 -0300

    Initial commit

---
+++ src/models/tarefaModel.js
@@ -0,0 +1,43 @@
+import prisma from "../../prisma/client.js";
+
+class TarefaModel {
+  getAll = async () => {
+    return await prisma.task.findMany();
+  }
+
+  create = async (descricao) => {
+    return await prisma.task.create({
+      data: {
+        descricao: descricao,
+      },
+    })
+  };
+
+  update = async (id, concluida) => {
+    try {
+      const tarefa = await prisma.task.update({
+        where: { id },
+        data: {
+          concluida: concluida !== undefined ? concluida : true,
+        }
+      });
+      return tarefa;
+    } catch (error) {
+      console.log("Error", error);
+      throw error
+    }
+  };
+
+  delete = async (id) => {
+    try {
+      const tarefaDeletada = await prisma.task.delete({
+        where: { id },
+      });
+      return tarefaDeletada;
+    } catch (error) {
+      console.error("Error ao deletar a tarefa!", error);
+      throw error;
+    }
+  };
+}
+export default new TarefaModel();
