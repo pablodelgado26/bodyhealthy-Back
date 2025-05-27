e2ac826fc1b4729e23d461276b0cf39a0cb9f7e1
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-05-27 08:32:40 -0300

    feat: depriving open routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -13,14 +13,14 @@ const router = express.Router()
 //Rotas de públicas
 
 router.use("/auth", authRouter)
-router.use("/posts", postRouter)
-router.use("/user", userRouter)
-router.use("/exercise", exerciseRoutes )
-router.use("/training", trainingRoutes)
 
 // Rotas privadas
 router.use(authMiddleware)
 
 router.use("/comment", commentRouter)
+router.use("/posts", postRouter)
+router.use("/user", userRouter)
+router.use("/exercise", exerciseRoutes )
+router.use("/training", trainingRoutes)
 
 export default router
